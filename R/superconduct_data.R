6509f22cf71fa098d17b3398a15651f0f5f52665
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2022-07-27 11:44:02 +0200

    Include abalone and superconduct datasets

---
+++ R/superconduct_data.R
@@ -0,0 +1,11 @@
+#' Superconductivity Data Set
+#'
+#' Two dataset contains data on 21263 superconductors and their relevant features.
+#'
+#' @format A dataframe with 21263 rows and 82 variables
+#'
+#' @source Kam Ham idieh, \email{khamidieh@gmail.com}, University of Pennsylvania, Statistics
+#'
+#' @examples
+#' data(superconduct)
+"superconduct"
