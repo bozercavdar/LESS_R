af990bdeef70950d456b9819e054217a681ef415
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2022-08-09 11:34:50 +0200

    Implement CoverTree
    Fix some documentation

--- R/less.R
+++ R/less.R
@@ -693,14 +693,60 @@ KDTree <- R6::R6Class(classname = "KDTree",
                         #' }
                         #'
                         #' @examples
-                        #' data(abalone)
-                        #' kdt <- KDTree$new(abalone)
                         #' res <- kdt$query(abalone[1:3,], k=2)
                         #' print(res)
                         query = function(query_X = private$X, k=1){
                           # query the tree for the k nearest neighbors
                           query <- as.matrix(query_X)
                           RANN::nn2(data = private$X, query = query, k = k)
+                        }
+                      ))
+
+#' @title CoverTree - Nearest Neighbor Search
+#'
+#' @description Wrapper R6 Class of FNN::get.knnx function that can be used for LESSRegressor and LESSClassifier
+#'
+#' @details The cover tree is O(n) space data structure which allows us to answer queries in the same O(log(n)) time as kd tree given a fixed intrinsic dimensionality.
+#' Templated code from \link{http://hunch.net/~jl/projects/cover_tree/cover_tree.html} is used.
+#'
+#' @param X An \strong{M x d} data.frame or matrix, where each of the \strong{M} rows is a point or a (column) vector (where \strong{d=1}).
+#'
+#' @return R6 Class of CoverTree
+#' @importFrom FNN get.knnx
+#' @seealso [FNN::get.knnx()]
+#' @export
+CoverTree <- R6::R6Class(classname = "CoverTree",
+                      private = list(
+                        X = NULL
+                      ),
+                      public = list(
+                        #' @description Creates a new instance of R6 Class of CoverTree
+                        #'
+                        #' @examples
+                        #' data(abalone)
+                        #' ct <- CoverTree$new(abalone)
+                        initialize = function(X = NULL) {
+                          private$X = X
+                        },
+                        #' @description Finds the p number of near neighbours for each point in an input/output dataset.
+                        #'
+                        #' @param query_X A set of \strong{N x d} points that will be queried against \code{X}. \strong{d}, the number of columns, must be the same as \code{X}.
+                        #' If missing, defaults to  \code{X}.
+                        #' @param k The maximum number of nearest neighbours to compute (deafults to 1).
+                        #'
+                        #' @return A \code{list} of length 2 with elements:\tabular{ll}{
+                        #'    \code{nn.idx} \tab A \strong{N x k} integer matrix returning the near neighbour indices. \cr
+                        #'    \tab \cr
+                        #'    \code{nn.dists} \tab A \strong{N x k} matrix returning the near neighbour Euclidean distances \cr
+                        #' }
+                        #'
+                        #' @examples
+                        #' res <- ct$query(abalone[1:3,], k=2)
+                        #' print(res)
+                        query = function(query_X = private$X, k=1){
+                          # query the tree for the k nearest neighbors
+                          query <- as.matrix(query_X)
+                          FNN::get.knnx(data = private$X, query = query, k = k, algorithm = "cover_tree")
                         }
                       ))
 
@@ -1932,7 +1978,7 @@ synthetic_sine_data <- cbind(X_sine, y_sine)
 
 #########################
 
-test <- function(data = abalone) {
+testFunc <- function(data = abalone) {
   # UNCOMMENT THIS CODE BLOCK TO PROFILE THE CODE AND SEE A PERFORMANCE ANALYSIS OF THE CODE
   # profvis::profvis({
   #
@@ -1957,25 +2003,21 @@ test <- function(data = abalone) {
   # mape <- MLmetrics::MAPE(preds, y_test)
   # cat("MAPE: ", mape, "\n")
 
-  hc <- KMeans$new()
-  hc$fit(X_train)
-  hc$set_random_state(5)
+  cat("Total number of training samples: ", nrow(X_train), "\n")
+  LESS <- LESSRegressor$new(random_state = 1, tree_method = function(data) CoverTree$new(X=data))
+  preds <- LESS$fit(X_train, y_train)$predict(X_test)
+  print(LESS)
+  print(head(matrix(c(y_test, preds), ncol = 2)))
+  mape <- MLmetrics::MAPE(preds, y_test)
+  cat("MAPE: ", mape, "\n")
 
-  # cat("Total number of training samples: ", nrow(X_train), "\n")
-  # LESS <- LESSRegressor$new(cluster_method = HierarchicalClustering$new())
-  # preds <- LESS$fit(X_train, y_train)$predict(X_test)
-  # print(LESS)
-  # print(head(matrix(c(y_test, preds), ncol = 2)))
-  # mape <- MLmetrics::MAPE(preds, y_test)
-  # cat("MAPE: ", mape, "\n")
-  #
-  # cat("Total number of training samples: ", nrow(X_train), "\n")
-  # LESS <- LESSRegressor$new()
-  # preds <- LESS$fit(X_train, y_train)$predict(X_test)
-  # print(LESS)
-  # print(head(matrix(c(y_test, preds), ncol = 2)))
-  # mape <- MLmetrics::MAPE(preds, y_test)
-  # cat("MAPE: ", mape, "\n")
+  cat("Total number of training samples: ", nrow(X_train), "\n")
+  LESS <- LESSRegressor$new(random_state = 1)
+  preds <- LESS$fit(X_train, y_train)$predict(X_test)
+  print(LESS)
+  print(head(matrix(c(y_test, preds), ncol = 2)))
+  mape <- MLmetrics::MAPE(preds, y_test)
+  cat("MAPE: ", mape, "\n")
 
 }
 
