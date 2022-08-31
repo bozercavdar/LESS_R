277d6041e3f2976a2bdec6481ed9e3f8841f77b2
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2022-08-31 13:57:10 +0200

    Implement data.superconduct function

---
+++ R/datasets.R
@@ -0,0 +1,10 @@
+#' @title Superconduct Dataset
+#'
+#' @description The dataset contains data on 21263 superconductors and their relevant features.
+#'
+#' @export
+data.superconduct = function(){
+  id <- "1rU3LlYpxSORlcKoPIniaxEdTyhfGWurN"
+  data <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id), sep = ",", header = FALSE)
+  return(data)
+}
