b09648d029221fbe6af8092f3c695ce148f2aedb
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2022-09-05 11:54:17 +0200

    remove testFunc()

--- R/tutorial.R
+++ R/tutorial.R
@@ -1,88 +1,14 @@
-#' @importFrom profvis profvis
-testFunc <- function(data = abalone) {
-  # UNCOMMENT THIS CODE BLOCK TO PROFILE THE CODE AND SEE A PERFORMANCE ANALYSIS OF THE CODE
-  # profvis::profvis({
-  #
-  # })
-
-  data <- iris
-  # data <- read.csv("C:/Users/ozerc/Desktop/data/algeria-fire-cls1.csv", sep = ",", header = TRUE)
-  # data[,-ncol(data)] <- sapply(data[,-ncol(data)], as.numeric)
-  # data <- data[,-c(2)]
-  # data <- data[,1:30]
-
-  # split_list <- train_test_split(data, test_size =  0.3, y_index = 1)
-  split_list <- train_test_split(data, test_size =  0.3)
-  X_train <- split_list[[1]]
-  X_test <- split_list[[2]]
-  y_train <- split_list[[3]]
-  y_test <- split_list[[4]]
-
-
-  # print(k_fold_cv(data = data, random_state = 2, model = LESSClassifier$new(), k = 3))
-
-  test_timing(2, X_train, y_train)
-
-  # str <- LESSClassifier$new()
-  # preds <- str$fit(X_train, y_train)$predict(X_test)
-  # example <- caret::confusionMatrix(data=factor(preds), reference = factor(y_test))
-  # print(example$overall["Accuracy"])
-
-  # profvis::profvis({
-  #   str <- LESSClassifier$new(multiclass = "ovr")
-  #   preds <- str$fit(X_train, y_train)$predict(X_test)
-  #   example <- caret::confusionMatrix(data=factor(preds), reference = factor(y_test))
-  #   print(example)
-  # })
-
-  # str <- LESSClassifier$new(multiclass = "ovr")
-  # # str <- KNeighborsClassifier$new()
-  # preds <- str$fit(X_train, y_train)$predict(X_test)
-  # example <- caret::confusionMatrix(data=factor(preds), reference = factor(y_test))
-  # print(example)
-
-  # y_index = 1
-  # X_train <- as.matrix(data[1:400,-y_index])
-  # y_train <- data[1:400,y_index]
-  # X_test <- as.matrix(data[401:nrow(data),-y_index])
-  # y_test <- data[401:nrow(data),y_index]
-
-  # model <- SVR$new()
-  # preds <- model$fit(X_train, y_train)$predict(X_test)
-  # print(head(matrix(c(y_test, preds), ncol = 2)))
-  # mape <- MLmetrics::MAPE(preds, y_test)
-  # cat("MAPE: ", mape, "\n")
-
-  # cat("Total number of training samples: ", nrow(X_train), "\n")
-  # LESS <- LESSRegressor$new(random_state = 1, tree_method = function(data) CoverTree$new(X=data))
-  # preds <- LESS$fit(X_train, y_train)$predict(X_test)
-  # print(LESS)
-  # print(head(matrix(c(y_test, preds), ncol = 2)))
-  # mape <- MLmetrics::MAPE(preds, y_test)
-  # cat("MAPE: ", mape, "\n")
-  #
-  # cat("Total number of training samples: ", nrow(X_train), "\n")
-  # LESS <- LESSRegressor$new(random_state = 1, global_estimator = LESSRegressor$new())
-  # preds <- LESS$fit(X_train, y_train)$predict(X_test)
-  # print(LESS)
-  # print(head(matrix(c(y_test, preds), ncol = 2)))
-  # mape <- MLmetrics::MAPE(preds, y_test)
-  # cat("MAPE: ", mape, "\n")
-
-  # labels <- c(6,5,4,6,5,5,4,4,5,5)
-  # testdata <- concrete[1:10,-3]
-  # testdata[,ncol(testdata)] <- labels
-  # X <- as.matrix(testdata[,-ncol(testdata)])
-  # y <- as.matrix(testdata[,ncol(testdata)])
-
-}
-
 #' @title Synthetic Sine Curve
 #'
 #' @description A simple function to generate n_samples from sine curve in the range (-10, 10) with some amplitude.
 #' The function returns the dataset (X, y), and plots the function (curve) along with the dataset (circles)
 #'
 #' @param n_samples Number of data points to be generated
+#'
+#' @importFrom grDevices dev.new
+#' @importFrom stats rnorm
+#' @importFrom stats runif
+#' @importFrom graphics par
 #'
 #' @export
 synthetic_sine_curve = function(n_samples=200) {
@@ -113,6 +39,7 @@ synthetic_sine_curve = function(n_samples=200) {
 #' @param y Response variables
 #' @param model_list List of models to be compared
 #'
+#' @importFrom graphics par
 #' @export
 comparison_plot = function(X, y, model_list){
   xlb <- floor(min(X)-1)
@@ -178,6 +105,8 @@ speed_test_classifiers = function(X, y){
 #' @param X Predictors
 #' @param y Response variables
 #'
+#' @importFrom graphics barplot
+#'
 #' @export
 test_timing = function(type = 1, X, y){
  if(type == 1){
@@ -200,5 +129,3 @@ test_timing = function(type = 1, X, y){
            names.arg = names(timings))
  }
 }
-
-
